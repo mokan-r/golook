0dfca4ea67dff4504bc1750b09ac1b30ca9100d7
mokan-r <mokan.roman8@gmail.com>
2023-05-18 18:44:27 +0700

    feat: removed commented lines

--- migrations/20230507044559_init_schema.up.sql
+++ migrations/20230507044559_init_schema.up.sql
@@ -7,31 +7,3 @@ CREATE TABLE IF NOT EXISTS commands (
                                         started_at TIMESTAMP NOT NULL,
                                         finished_at TIMESTAMP NOT NULL
 );
--- CREATE TABLE IF NOT EXISTS monitored_files (
---                                                id SERIAL PRIMARY KEY,
---                                                monitored_dir_id INTEGER NOT NULL,
---                                                path TEXT NOT NULL,
---                                                is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
---                                                include_regexp TEXT,
---                                                exclude_regexp TEXT,
---                                                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---                                                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---                                                FOREIGN KEY (monitored_dir_id) REFERENCES monitored_directories (id) ON DELETE CASCADE
--- );
--- CREATE TABLE IF NOT EXISTS commands (
---                                         id SERIAL PRIMARY KEY,
---                                         monitored_dir_id INTEGER NOT NULL,
---                                         command TEXT NOT NULL,
---                                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---                                         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---                                         FOREIGN KEY (monitored_dir_id) REFERENCES monitored_directories (id) ON DELETE CASCADE
--- );
---
--- CREATE TABLE IF NOT EXISTS command_executions (
---                                                   id SERIAL PRIMARY KEY,
---                                                   command_id INTEGER NOT NULL,
---                                                   output TEXT,
---                                                   exit_code INTEGER,
---                                                   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---                                                   FOREIGN KEY (command_id) REFERENCES commands (id) ON DELETE CASCADE
--- );
